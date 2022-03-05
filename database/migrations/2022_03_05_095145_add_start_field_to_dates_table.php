dd86cb82fa198c8bc61e0a5e7560f55b3cd7e843
hossein sattari <ho3einsss84@gmail.com>
2022-03-05 13:37:34 +0330

    sd

--- database/migrations/2022_03_05_095145_add_start_field_to_dates_table.php
+++ database/migrations/2022_03_05_095145_add_start_field_to_dates_table.php
@@ -26,7 +26,7 @@ class AddStartFieldToDatesTable extends Migration
     public function down()
     {
         Schema::table('dates', function (Blueprint $table) {
-            //
+            $table->date('start');
         });
     }
 }
